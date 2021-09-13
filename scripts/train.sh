e6537773f853ada82eccc2ca4b0d30d7d737942e
clay <sos06096sos06096@gmail.com>
2021-09-13 17:44:17 +0800

    to fix that --resume cannot load outputdir checkpoint and add TEST.EVAL_PERIOD

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,2 +1,4 @@
 python tools/train_net.py --config-file "/media/t2-503-c/Data4/ClayYou/detectron2-DA/configs/swda_rcnn_clg_R_50_FPN_1x.yaml" --num-gpus 1 \
 VIS_PERIOD 1
+# python tools/train_net.py --config-file "/media/t2-503-c/Data4/ClayYou/detectron2-DA/configs/swda_rcnn_clg_R_50_FPN_1x.yaml" --num-gpus 1 \
+# --resume VIS_PERIOD 1 OUTPUT_DIR "outputs/output-21-09-11_20-30/"
